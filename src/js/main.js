e1f6b924ac665826f165459b6ee9abf087c7b987
Per Lundgren <per.lundgren@vinnovera.se>
2015-11-19 10:25:57 +0100

    latest additions
    Signed-off-by: Per Lundgren <per.lundgren@vinnovera.se>

--- src/js/main.js
+++ src/js/main.js
@@ -1,69 +1,153 @@
-$(function() {
+!function(w, d, $) {
 
-	var tabBar = $('.tab-bar'),
-		tabs = $('.tab'),
-		addNew = $('#add-new');
+	var $tabBar, $tabs, iAmSuper = false. iAmDouble = false;
 
-	
-	function tabClick(){
-		var tabs = $('.tab');
+	$(d).on('ready', function() {
+		$tabBar = $('.tab-bar'),
+		$tabs = $('.tab');
 
-		tabs.each(function(){
-				var me = $(this),
-					content = me.find('.tab-content'),
-					close = me.find('.close'),
-					pin = me.find('.pin');
+		sortTabs();
+		bindEvents();
+	});
 
-				content.on('click', function(){
-					tabs.removeClass('active');
-					me.addClass('active');
-				});
+	function bindEvents() {
+		$(d).on('click', '.add-new', onAddNewClick);
+		$(d).on('click', '.close-all', onCloseAllClick);
+		$(d).on('click', '.close-all-but-active', onKeepActiveClick);
+		$(d).on('click', '.close-all-but-pinned', onKeepPinnedClick);
 
-				close.on('click', function(){
-					me.remove();
-				});
+		//Tab clicks
+		$(d).on('click', '.tab-content', onTabContentClick);
+		$(d).on('click', '.close', onCloseClick);
+		$(d).on('click', '.pin', onPinClick);
+	}
 
-				pin.on('click', function(){
-					me.toggleClass('pinned');
-				});
+	function sortTabs(){
+		$tabBar.sortable({
+				axis: "x",
+				placeholder: "tab placeholder",
+				sort: function( event, ui ) {
+					$tabBar.addClass('sorting-tab');
+					ui.item.removeClass('moved');
+					ui.item.addClass('sorting');
+				},
+				stop: function( event, ui ) {
+					$tabBar.removeClass('sorting-tab');
+					ui.item.addClass('moved');
+					ui.item.attr('style','');
+					ui.item.removeClass('sorting');
+				},
 		});
+	
+		$tabs.disableSelection();
+	}
+
+	function onPinClick(e){
+		e.preventDefault();
+		var $tab = $(e.target).closest('.tab');
+
+		$tab.toggleClass('pinned');
+	}
+
+	function onTabContentClick(e){
+		e.preventDefault();
+		var $tab = $(e.target).closest('.tab');
+
+		$('.tab').removeClass('active');
+		$tab.addClass('active');
+		resizeTabBar();
+	}
+
+	function onCloseClick(e){
+		e.preventDefault();
+		var $tab = $(e.target).closest('.tab');
+
+		$tab.addClass('closing');
+		setTimeout(function(){ $tab.remove(); },500);
+		resizeTabBar();
+	}
+
+	function onAddNewClick(e){
+
+			e.preventDefault();
+			var randomIndex = Math.floor(Math.random() * $tabs.length),
+				tabClone = $tabs.eq(randomIndex);
+				
+			tabClone.clone().removeClass('active pinned closing').appendTo( $tabBar );
+			resizeTabBar();
+	}
+
+	function onCloseAllClick(e){
+			e.preventDefault();
+			$('.tab').addClass('closing');
+			setTimeout(function(){
+				$('.tab').remove();
+				cleanTabBar();
+				//resizeTabBar();
+			},500);
+			
+	}
+
+	function onKeepActiveClick(e){
+			e.preventDefault();
+			$('.tab:not(.active)').addClass('closing');
+			setTimeout(function(){
+				$('.tab:not(.active)').remove();
+				if (!iAmSuper || !iAmDouble) {
+					cleanTabBar();
+					resizeTabBar();
+				}
+			},500);
+			
+	}
+
+	function onKeepPinnedClick(e){
+			e.preventDefault();
+			$('.tab:not(.pinned)').addClass('closing');
+			setTimeout(function(){
+				$('.tab:not(.pinned)').remove();
+				if (!iAmSuper || !iAmDouble) {
+					cleanTabBar();
+					resizeTabBar();
+				}
+			},500);
+	}
+
+	function cleanTabBar(){
+		$tabBar.removeClass('condensed super-condensed');
+		iAmSuper = false;
+		iAmDouble = false;
 	}
 
 	function resizeTabBar(){
-		var tabsWidth = tabs.outerWidth() * tabs.length;
-		tabBar.width(tabsWidth);
-	}
+		var
+			viewportWidth = $(w).width(),
+			tabsWidth = ($('.tab').width() - 32) * $('.tab').length,
+			doubleWidth = viewportWidth * 2;
 
-	function addnewClick(){
-		addNew.on('click', function(){
-			var randomIndex = Math.floor(Math.random() * tabs.length),
-				tabClone = tabs.eq(randomIndex);
-				
-			tabClone.clone().removeClass('active').appendTo( '.tab-bar' );
-			tabClick();
-			resizeTabBar();
-		});
-	}
+		/*var tabsWidth = 0;
+		$('.tab').each(function(){
+			tabsWidth = tabsWidth + ($(this).width() - 32);
+		});*/
 
-	function tabInit(){
+		/*console.log('normal: ' + tabsWidth);
+
+
+		if(tabsWidth >= doubleWidth || iAmSuper) {
+			$tabBar.addClass('super-condensed').removeClass('condensed');
+			console.log('double: ' + tabsWidth);
+			iAmSuper = true;
+		} else if(tabsWidth > viewportWidth && tabsWidth < doubleWidth && !iAmSuper) {
+			$tabBar.addClass('condensed').removeClass('super-condensed');
+			console.log('condensed: ' + tabsWidth);
+			iAmDouble = true;
+		}else{
+			iAmSuper = false;
+			iAmDouble = false;
+		}*/
 		
-		$( '.tab-bar' ).sortable({
-				axis: "x"
-				/*sort: function( event, ui ) {
-					console.log(ui);
-				}*/
-		});
-	
-		tabs.disableSelection();
-		tabClick();
-		addnewClick();
-
+		$('.tab-bar').width(tabsWidth);
 	}
 
-	tabInit();
-
-
-
-
 	
-});
+}(window, document, jQuery);
